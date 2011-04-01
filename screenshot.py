331a5ed14ed8c2c53368dac2ad6cbe5f54d4018a
Denis Subbotin <tr0n@yandex.ru>
2011-04-01 16:08:16 +0400

    поправил скриншотер. он теперь кросс-платформеный (правда под виндой не тестил), обновил ридми, добавил файл с тестом.

--- screenshot.py
+++ screenshot.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import gtk
-def get_screenshot():
-	screenshot = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, 0, 8, gtk.gdk.screen_width(), gtk.gdk.screen_height())
-	screenshot.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), 0, 0, 0, 0, gtk.gdk.screen_width(), gtk.gdk.screen_height())
-#	screenshot.save('temp.png', 'png') # для отладки. сохраняет в файл
-	return screenshot
+from PIL import Image
+import sys
+
+def get_screenshot_linux():
+	sshot = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, 0, 8, gtk.gdk.screen_width(), gtk.gdk.screen_height())
+	sshot.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), 0, 0, 0, 0, gtk.gdk.screen_width(), gtk.gdk.screen_height())
+#	sshot.save('temp.png', 'png') # для отладки. сохраняет в файл
+	img = Image.frombuffer("RGB", (sshot.get_width(), sshot.get_height()), sshot.get_pixels(), "raw", "RGB",sshot.get_rowstride(),1)
+	return img
 	
+def get_screenshot_win():
+	from PIL import ImageGrab
+	img = ImageGrab.grab()
+	return img
+
+def get_screenshot():
+	if sys.platform == 'win32':
+		return get_screenshot_win()
+	else:
+		return get_screenshot_linux()
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from koordinat_seacher import find_template
+from PIL import Image
+from screenshot import get_screenshot
+search_image = get_screenshot()
+template_image = Image.open("/home/mr_tron/downloads/template.png")
+print find_template(search_image, template_image)
+
